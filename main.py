66fb0d7eca899c74b81e2823d732141179b33dcd
ahirota <avjhirota@gmail.com>
2025-07-22 21:45:01 -0700

    [AIAGENT_1_5] Refactor to Check for multiple lines

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    verboseFlag = False
+    verbose_flag = False
 
     inputs = sys.argv[1:]
 
     if not inputs:
         raise Exception("Program requires a prompt.\nPlease use format: python main.py \"your prompt here\"\nExample: python main.py \"What is the meaning of life?\"")
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=inputs[0])]),
-    ]
-
     if "--verbose" in inputs:
-        verboseFlag = True
+        verbose_flag = True
+
+    args = []
+    for arg in inputs:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -28,7 +35,7 @@ def main():
     )
 
     print(response.text)
-    if verboseFlag:
+    if verbose_flag:
         print(f"User prompt: {inputs[0]}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
